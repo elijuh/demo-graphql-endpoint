8d4d243ed7bae318e8ca3ec239055b92c24c9a61
elijuh <elijuhbusiness@gmail.com>
2024-11-25 12:29:55 -0500

    Added 'addAuthor' mutation.

--- src/main/java/dev/elijuh/endpoint/controller/AuthorController.java
+++ src/main/java/dev/elijuh/endpoint/controller/AuthorController.java
@@ -5,6 +5,7 @@ import dev.elijuh.endpoint.entity.Author;
 import dev.elijuh.endpoint.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -27,4 +28,12 @@ public class AuthorController {
     public Optional<Author> authorById(@Argument Long id) {
         return authorRepository.findById(id);
     }
+
+    @MutationMapping
+    public Author addAuthor(@Argument AuthorInput authorInput) {
+        Author author = new Author(authorInput.name);
+        return authorRepository.save(author);
+    }
+
+    public record AuthorInput(String name) {}
 }
